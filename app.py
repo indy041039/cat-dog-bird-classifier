9df1e19fcca6aca29b46228bc75e5d76c44dff30
indyspace <ardong_suwan@hotmail.com>
2021-02-15 21:37:47 +0700

    make it better

--- app.py
+++ app.py
@@ -13,11 +13,16 @@ def home():
 @app.route('/prediction')
 def classifier():
     img_url = request.values['img_url']
+    print('recieve')
     img = tf.image.decode_jpeg(requests.get(img_url).content, channels=3, name="jpeg_reader")
+    print('decode')
     resized_img = tf.image.resize(img, (224,224))
+    print('resize')
     image = tf.expand_dims(resized_img, axis=0)
+    print('expand')
     model = tf.keras.applications.ResNet50(include_top=False, weights='imagenet')
     features = model(image).numpy()
+    print('extract')
     flat_features = features.flatten()
 
     model = joblib.load('model.pkl')
