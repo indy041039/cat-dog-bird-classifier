014d8da0ef51f4882adb3eff6333ea99bde20541
indyspace <ardong_suwan@hotmail.com>
2021-02-24 14:10:47 +0700

    make it better

--- app.py
+++ app.py
@@ -16,18 +16,6 @@ def home():
 @app.route('/prediction')
 def classifier():
     img_url = request.values['p_image_url']
-    #Convert image to numpy array
-    # response = requests.get(img_url)
-    # image_bytes = io.BytesIO(response.content)
-    # img = PIL.Image.open(image_bytes)
-    # img = np.array(img)
-    # #Resize image into 224 x 224 x 3
-    # resized_img = tf.image.resize(img, (224,224))
-    # image = tf.expand_dims(resized_img, axis=0)
-    # model = tf.keras.applications.ResNet50(include_top=False, weights='imagenet')
-    # #Extract features with ResNet50
-    # features = model(image).numpy()
-    # flat_features = features.flatten()
     a = cv.image(img_url)
     feat = a.getmobilenet()
     model = joblib.load('model_mobilenet.pkl')
